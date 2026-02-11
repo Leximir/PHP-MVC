b78f089fae4fc9abccf6256e6f893ab2aa24e112
Leximir <aleksa.stankovic@proton.me>
2026-02-11 19:27:15 +0100

    Made class Router.php

--- core/Router.php
+++ core/Router.php
@@ -4,15 +4,21 @@ namespace app\core;
 
 class Router
 {
+    public Request $request; // Čuva Request objekat (trenutni HTTP zahtjev) da Router može čitati path i metodu.
+    protected array $routes = []; // Mapa ruta: čuva definisane rute (path -> callback) po HTTP metodi.
 
-    protected array $routes = [];
-    public function get($path, $callback) // Accepts path and callback function
+    public function __construct(Request $request)
     {
-        $this->routes['get'][$path] = $callback;
-//        Example:
-//        'get' => [
-//            '/' => $callback
-//        ]
+        $this->request = $request; // Prima i sprema Request objekat koji Router koristi za čitanje path/metode.
     }
-    
+    public function get($path, $callback) // Registruje GET rutu: povezuje URL putanju sa callback funkcijom.
+    {
+        $this->routes['get'][$path] = $callback; // Sprema callback pod datom putanjom u GET rute.
+    }
+
+    public function resolve()
+    {
+        $this->request->getPath(); // Uzima trenutni path i na osnovu njega pronalazi i izvršava odgovarajuću rutu.
+    }
+
 }

23c5e8dadbf0a9ddfc777e54b03b4624fa57f199
Leximir <aleksa.stankovic@proton.me>
2026-02-11 19:00:56 +0100

    Made basic function to create paths with get request

--- core/Router.php
+++ core/Router.php
@@ -5,4 +5,14 @@ namespace app\core;
 class Router
 {
 
+    protected array $routes = [];
+    public function get($path, $callback) // Accepts path and callback function
+    {
+        $this->routes['get'][$path] = $callback;
+//        Example:
+//        'get' => [
+//            '/' => $callback
+//        ]
+    }
+    
 }

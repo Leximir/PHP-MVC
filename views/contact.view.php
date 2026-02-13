1c347c1c020be1bba77945b1b13142ed6f752920
Leximir <aleksa.stankovic@proton.me>
2026-02-13 23:31:23 +0100

    Generating some frontend with bootstrap

--- views/contact.view.php
+++ views/contact.view.php
@@ -3,3 +3,21 @@
 ?>
 
 <h1>Contact</h1>
+
+<form>
+    <div class="mb-3">
+        <label>Subject</label>
+        <input name="subject" type="text" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Email</label>
+        <input name="email" type="email" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Body</label>
+        <textarea name="body" type="text" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>

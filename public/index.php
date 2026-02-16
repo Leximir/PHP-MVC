eabf208e9d0b407d3c6f796c8e4b63a4cf65257b
Leximir <aleksa.stankovic@proton.me>
2026-02-16 11:33:13 +0100

    Defined new paths for authentication

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
 
@@ -11,5 +12,10 @@ $app = new Application(dirname(__DIR__)); // Kreira instancu aplikacije i postav
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'index']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run(); // Pokretanje aplikacije: router pokušava da match-uje rutu i izvrši odgovarajući handler
